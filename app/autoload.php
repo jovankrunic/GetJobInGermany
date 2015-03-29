b6bf48bfdaf6e53fe5dde7ef9c4baafb0f63df2f
Jovan Krunic <jovan.krunic@gmail.com>
2015-03-29 17:12:22 +0200

    change font size for sidebar links

--- app/autoload.php
+++ app/autoload.php
@@ -10,4 +10,9 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
+/* Jovan 2015-03-27 */
+set_include_path(__DIR__.'/../vendor'.PATH_SEPARATOR.get_include_path());
+    require_once __DIR__.'/../vendor/Zend/Loader/Autoloader.php';
+    Zend_Loader_Autoloader::getInstance();
+
 return $loader;
